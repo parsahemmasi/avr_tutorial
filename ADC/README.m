98df34607e90baf994235e0e7c1ba93892a0052b
Seyed Parsa Hemmasi <parsahemmasi@gmail.com>
2022-07-24 16:59:02 +0430

    Update README.m

--- ADC/README.m
+++ ADC/README.m
@@ -1,1 +1,6 @@
-hello
+# 7Segment Counter
+
+In this project there is a common cathode-seven segment connected to ATMega32 microcontroller that the digit shown on seven segment increases when button get pushed.
+
+<img src=""  />
+
