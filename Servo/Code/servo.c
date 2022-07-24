0d3d5850adae6423af05104e5534789ec2cbaf4e
Seyed Parsa Hemmasi <parsahemmasi@gmail.com>
2022-07-24 17:57:06 +0430

    Add files via upload

---
+++ Servo/Code/servo.c
@@ -0,0 +1,75 @@
+#include <mega32.h>
+#include <delay.h>
+#include <alcd.h>
+#include <stdio.h>
+
+#define ADC_VREF_TYPE ((0<<REFS1) | (0<<REFS0) | (0<<ADLAR))
+
+#define button  PINB.2
+#define servo_value OCR1A
+
+char matn[40];
+float pot_angle;
+
+unsigned int read_adc(unsigned char adc_input)
+{
+    ADMUX=adc_input | ADC_VREF_TYPE;
+    delay_us(10);
+    ADCSRA|=(1<<ADSC);
+    while ((ADCSRA & (1<<ADIF))==0);
+    ADCSRA|=(1<<ADIF);
+    return ADCW;
+}
+void set_servo_angle(float angle)
+{
+    float buffer;
+    
+    buffer=((angle/180)*1752)+388;
+    servo_value=(int)buffer;  
+}
+
+void main(void)
+{
+    DDRA=(0<<DDA7) | (0<<DDA6) | (0<<DDA5) | (0<<DDA4) | (0<<DDA3) | (0<<DDA2) | (0<<DDA1) | (0<<DDA0);
+    PORTA=(0<<PORTA7) | (0<<PORTA6) | (0<<PORTA5) | (0<<PORTA4) | (0<<PORTA3) | (0<<PORTA2) | (0<<PORTA1) | (0<<PORTA0);
+
+    DDRB=(0<<DDB7) | (0<<DDB6) | (0<<DDB5) | (0<<DDB4) | (0<<DDB3) | (0<<DDB2) | (0<<DDB1) | (0<<DDB0);
+    PORTB=(0<<PORTB7) | (0<<PORTB6) | (0<<PORTB5) | (0<<PORTB4) | (0<<PORTB3) | (1<<PORTB2) | (0<<PORTB1) | (0<<PORTB0);
+
+    DDRC=(1<<DDC7) | (1<<DDC6) | (1<<DDC5) | (1<<DDC4) | (1<<DDC3) | (1<<DDC2) | (1<<DDC1) | (1<<DDC0);
+    PORTC=(0<<PORTC7) | (0<<PORTC6) | (0<<PORTC5) | (0<<PORTC4) | (0<<PORTC3) | (0<<PORTC2) | (0<<PORTC1) | (0<<PORTC0);
+    
+    DDRD=(0<<DDD7) | (0<<DDD6) | (1<<DDD5) | (0<<DDD4) | (0<<DDD3) | (0<<DDD2) | (0<<DDD1) | (0<<DDD0);
+    PORTD=(0<<PORTD7) | (0<<PORTD6) | (0<<PORTD5) | (0<<PORTD4) | (0<<PORTD3) | (0<<PORTD2) | (0<<PORTD1) | (0<<PORTD0);
+
+    ASSR=0<<AS2;
+
+    TCCR1A=(1<<COM1A1) | (0<<COM1A0) | (0<<COM1B1) | (0<<COM1B0) | (1<<WGM11) | (0<<WGM10);
+    TCCR1B=(0<<ICNC1) | (0<<ICES1) | (1<<WGM13) | (1<<WGM12) | (0<<CS12) | (1<<CS11) | (0<<CS10);
+    ICR1H=0x4E;
+    ICR1L=0x1F; 
+     
+    TIMSK=(0<<OCIE2) | (0<<TOIE2) | (0<<TICIE1) | (0<<OCIE1A) | (0<<OCIE1B) | (0<<TOIE1) | (0<<OCIE0) | (0<<TOIE0);
+
+    ADMUX=ADC_VREF_TYPE;
+    ADCSRA=(1<<ADEN) | (0<<ADSC) | (0<<ADATE) | (0<<ADIF) | (0<<ADIE) | (1<<ADPS2) | (0<<ADPS1) | (0<<ADPS0);
+    SFIOR=(0<<ADTS2) | (0<<ADTS1) | (0<<ADTS0);
+
+    lcd_init(16);
+
+    while (1)
+    {
+        pot_angle=read_adc(0);
+        pot_angle=(pot_angle/1023)*180;    
+    
+        sprintf(matn,"%d   ",(int)pot_angle);
+        lcd_gotoxy(0,0);
+        lcd_puts(matn);
+        
+        if(button==0)
+        {
+            while(button==0);
+            set_servo_angle(pot_angle);
+        }
+    }
+}
